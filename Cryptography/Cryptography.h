92edd0b7a1249abdf2aa11cac92fddced65707c8
AppDeveloper428 <voidilov@gmail.com>
2018-05-20 12:16:23 +0300

    Initial Commit

---
+++ Cryptography/Cryptography.h
@@ -0,0 +1,19 @@
+//
+//  Cryptography.h
+//  Cryptography
+//
+//  Created by Данил Войдилов on 20.05.2018.
+//  Copyright © 2018 Данил Войдилов. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Cryptography.
+FOUNDATION_EXPORT double CryptographyVersionNumber;
+
+//! Project version string for Cryptography.
+FOUNDATION_EXPORT const unsigned char CryptographyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cryptography/PublicHeader.h>
+
+
